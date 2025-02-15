02ded3908a8fc1bb249cc3403764b96814666a31
jayjith <jayjith0@gmail.com>
2025-02-15 15:51:18 +0530

    Me

--- Project12.py
+++ Project12.py
@@ -3,3 +3,11 @@ import os
 import datetime
 
 df = pd.read_csv('Project12BSSWelcomeCallWithoutRD140225.csv', header=None)
+
+
+date_column_name = 2
+df [date_column_name] = pd.to_datetime(df[date_column_name], errors='coerce').dt.strftime('%d/%m')
+column_name = 3
+unique_values = df[column_name].unique
+outputdirectory = 'test'
+os.makedirs(outputdirectory, exist_ok=True)
